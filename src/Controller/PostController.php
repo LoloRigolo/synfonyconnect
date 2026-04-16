00cf4c7e54e1f5590a98576ffd6c624ae4184724
LoloRigolo <lolocritiqueurs21@gmail.com>
2026-04-16 14:04:43 +0200

    fix: rule for new post nav btn and in the controller

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -10,12 +10,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-//use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class PostController extends AbstractController
 {
     #[Route('/post/nouveau', name: 'app_post_new')]
-    //#[IsGranted('ROLE_USER')] utilisable plus tard
+    #[IsGranted('ROLE_USER')]
     public function new(Request $request, EntityManagerInterface $em, UserRepository $userRepository): Response
     {
         $post = new Post();
